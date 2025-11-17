05b5065144726e6504afd3778ba365071901d972
jefrycent <162528867+jefrycent19@users.noreply.github.com>
2025-11-16 23:31:04 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,17 @@
 <?php
-$DB_HOST = getenv("DB_HOST");
-$DB_USER = getenv("DB_USER");
-$DB_PASS = getenv("DB_PASS");
-$DB_NAME = getenv("DB_NAME");
-$DB_PORT = getenv("DB_PORT");
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
 
-$CEDULA_PROGRAMADOR = getenv("CEDULA_PROGRAMADOR");
-$CLAVE_AES = getenv("CLAVE_AES");
-
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die(json_encode([
         "exito" => false,
-        "mensaje" => "Error de conexión: " . $conn->connect_error,
-        "datos" => null
+        "mensaje" => "Error en la conexión: " . $conn->connect_error
     ]));
 }
+
+$conn->set_charset("utf8");
 ?>
